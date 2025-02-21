129ff14a8ec56e28493e6cc2304d52acf5720116
Arya Wiratama <aryawiratama2401@gmail.com>
2025-02-21 13:02:54 +0700

    post

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from flask import Flask, render_template, request
 from shortUrl import short_url
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/')
 def main():
-    if request == 'POST':
-        short_url()
     return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def convert_url():
+    get_url = request.form.get("email")    
+    print(get_url)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
